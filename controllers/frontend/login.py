e54bae2d75b7dd69210d8f222d5ebf1f255a6144
Sokhavuth <vuthdevelop@gmail.com>
2022-08-25 16:23:56 +0700

    Make it better

--- controllers/frontend/login.py
+++ controllers/frontend/login.py
@@ -26,7 +26,7 @@ class Login:
         sessionid = request.get_cookie('sessionid', secret=self.secret_key)
         print(sessionid)
         print("test")
-        encoded_jwt = self.redis.get(sessionid) 
+        encoded_jwt = self.redis.get(request.get_cookie('sessionid', secret=self.secret_key)) 
         #print(encoded_jwt)
         try:
             payload = jwt.decode(encoded_jwt, self.secret_key, algorithms=["HS256"])
